3d10f94718e17875e562e3716af360eed4a75e34
Memmana <48010705+Memmana@users.noreply.github.com>
2020-09-11 17:02:10 +0530

    Add files via upload

---
+++ Assignment Day 3&4/image.js
@@ -0,0 +1,23 @@
+// QUESTION 1
+
+function displayImage() 
+{
+  var im1=document.getElementById('img1');
+  const newurl="images/avengers-wallpapers-.jpg";
+  im1.src=newurl;
+  
+}
+function displayImage2() 
+{
+  const im2=document.getElementById('img1');
+  const newurl="images/download.jpg";
+  im2.src=newurl;
+ 
+}
+function displayImage3() 
+{
+  const im3=document.getElementById('img1');
+  const newurl="images/cool-chicks-fb-photo.jpg";
+  im3.src=newurl;
+ 
+}
